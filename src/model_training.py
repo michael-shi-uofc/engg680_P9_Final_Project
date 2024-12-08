851f74d8a8af4a165a032d52ef94c95013204085
michael-shi-uofc <mjsshi@gmail.com>
2024-12-08 00:38:31 -0700

    added model training file

---
+++ src/model_training.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from keras.api.layers import Dense, Dropout
+from keras.api.models import Sequential
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, confusion_matrix
